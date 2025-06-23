1ac0b9137d602280ff7d7186aa65939a2b0707de
rmluck <rohandm99@yahoo.com>
2025-06-23 10:54:52 -0700

    Squares of a Sorted Array, 3Sum

---
+++ problems/3sum.py
@@ -0,0 +1,71 @@
+"""
+Problem 15: 3Sum (https://leetcode.com/problems/3sum/)
+- Difficulty: Medium
+- Categories: Array, Sorting
+- Technique: Two Pointers
+
+Given an integer array `nums`, return all the triplets `[nums[i], nums[j], nums[k]]` such that `i != j`, `i != k`, and `j != k`, and `nums[i] + nums[j] + nums[k] == 0`.
+
+Notice that the solution set must not contain duplicate triplets.
+
+Constraints:
+- 3 <= nums.length <= 3000
+- 10^5 <= nums[i] <= 10^5
+"""
+
+
+def three_sum(nums: list[int]) -> list[list[int]]:
+    triplets = []
+    nums.sort()
+
+    for i in range(len(nums) - 2):
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+
+        left = i + 1
+        right = len(nums) - 1
+
+        while left < right:
+            three_sum = nums[i] + nums[left] + nums[right]
+            
+            if three_sum < 0:
+                left += 1
+            elif three_sum > 0:
+                right -= 1
+            else:
+                triplets.append([nums[i], nums[left], nums[right]])
+
+                while left < right and nums[left] == nums[left + 1]:
+                    left += 1
+                while left < right and nums[right] == nums[right - 1]:
+                    right -= 1
+                left += 1
+                right -= 1
+
+    return triplets
+
+# Example 1
+assert three_sum([-1, 0, 1, 2, -1, -4]) == [[-1, -1, 2], [-1, 0, 1]]
+
+# Example 2
+assert three_sum([0, 1, 1]) == []
+
+# Example 3
+assert three_sum([0, 0, 0]) == [[0, 0, 0]]
+
+# Additional Test Cases
+assert three_sum([-2, 0, 1, 1, 2]) == [[-2, 1, 1], [-2, 0, 2]]
+assert three_sum([-4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4]) == [[-4, 1, 3], [-4, 2, 2], [-2, -2, 4], [-2, 0, 2], [-2, 1, 1]]
+assert three_sum([3, 0, -4, -1, -2, 1, 2]) == [[-4, 1, 3], [-2, -1, 3], [-2, 0, 2], [-1, 0, 1]]
+assert three_sum([0, 0, 0, 0]) == [[0, 0, 0]]
+assert three_sum([-1, 0, 1, 0]) == [[-1, 0, 1]]
+assert three_sum([-1, -1, -1, 2]) == [[-1, -1, 2]]
+assert three_sum([-2, 0, 0, 2, 2]) == [[-2, 0, 2]]
+assert three_sum([-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4]) == [[-4, 0, 4], [-4, 1, 3], [-3, -1, 4], [-3, 0, 3], [-3, 1, 2], [-2, -1, 3], [-2, 0, 2], [-1, -1, 2], [-1, 0, 1]]
+assert three_sum([1, 2, -2, -1]) == []
+assert three_sum([-2, -1, 1, 2]) == [[-2, -1, 3], [-2, 0, 2], [-1, 0, 1]]
+
+# Time Complexity: O(n^2)
+# Space Complexity: O(n)
+# Runtime: 573 ms, faster than 61.67% of Python3 online submissions
+# Memory: 20.48 MB, less than 90.02% of Python3 online submissions
---
+++ problems/squares_of_a_sorted_array.py
@@ -0,0 +1,38 @@
+"""
+Problem 977: Squares of a Sorted Array (https://leetcode.com/problems/squares-of-a-sorted-array/)
+- Difficulty: Easy
+- Categories: Array, Sorting
+- Technique: Two Pointers
+
+Given an integer array `nums` sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+Constraints:
+- 1 <= nums.length <= 10^4
+- -10^4 <= nums[i] <= 10^4
+- nums is sorted in non-decreasing order.
+"""
+
+
+def squares_of_a_sorted_array(nums: list[int]) -> list[int]:
+    for i in range(len(nums)):
+        nums[i] = nums[i] ** 2
+    
+    return sorted(nums)
+
+# Example 1
+assert squares_of_a_sorted_array([-4, -1, 0, 3, 10]) == [0, 1, 9, 16, 100]
+
+# Example 2
+assert squares_of_a_sorted_array([-7, -3, 2, 3, 11]) == [4, 9, 9, 49, 121]
+
+# Additional Test Cases
+assert squares_of_a_sorted_array([-5, -3, -2, -1]) == [1, 4, 9, 25]
+assert squares_of_a_sorted_array([1, 2, 3, 4]) == [1, 4, 9, 16]
+assert squares_of_a_sorted_array([-2, 0, 2]) == [0, 4, 4]
+assert squares_of_a_sorted_array([-1]) == [1]
+assert squares_of_a_sorted_array([0]) == [0]
+
+# Time Complexity: O(n log n)
+# Space Complexity: O(1)
+# Runtime: 6 ms, faster than 83.87% of Python3 online submissions
+# Memory: 19.45 MB, less than 62.37% of Python3 online submissions
